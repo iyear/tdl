8b8c4ecb7a36b1c0b2d27c3d8115a58a03c14ee8
iyear <ljyngup@gmail.com>
2022-09-04 11:02:50 +0800

    fix(downloader): write files concurrently

--- pkg/downloader/write_at.go
+++ pkg/downloader/write_at.go
@@ -3,15 +3,20 @@ package downloader
 import (
 	"github.com/jedib0t/go-pretty/v6/progress"
 	"os"
+	"sync"
 )
 
 // writeAt wrapper for file to use progress bar
 type writeAt struct {
+	mu      sync.Mutex
 	f       *os.File
 	tracker *progress.Tracker
 }
 
 func (w *writeAt) WriteAt(p []byte, off int64) (int, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
 	at, err := w.f.WriteAt(p, off)
 	if err != nil {
 		w.tracker.MarkAsErrored()
