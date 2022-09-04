f0913f5aaa3131b3081061d3b6924712a8da5110
iyear <ljyngup@gmail.com>
2022-09-04 18:51:48 +0800

    feat(utils): add path exists

--- pkg/utils/fs.go
+++ pkg/utils/fs.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -11,4 +12,9 @@ var FS = fs{}
 
 func (f fs) GetNameWithoutExt(path string) string {
 	return strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+}
+
+func (f fs) PathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
 }
