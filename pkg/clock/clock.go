246e70e986f5be10b9b1b8f2738df13512f31405
iyear <ljyngup@gmail.com>
2022-09-17 23:35:59 +0800

    feat(pkg): add network clock

---
+++ pkg/clock/clock.go
@@ -0,0 +1,48 @@
+package clock
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"github.com/gotd/td/clock"
+	"time"
+)
+
+const defaultHost = "pool.ntp.org"
+
+type networkClock struct {
+	offset time.Duration
+}
+
+func (n *networkClock) Now() time.Time {
+	return time.Now().Add(n.offset)
+}
+
+func (n *networkClock) Timer(d time.Duration) clock.Timer {
+	return clock.System.Timer(d)
+}
+
+func (n *networkClock) Ticker(d time.Duration) clock.Ticker {
+	return clock.System.Ticker(d)
+}
+
+// New default ntp host is 'pool.ntp.org'
+func New(ntpHost ...string) (clock.Clock, error) {
+	var host string
+	switch len(ntpHost) {
+	case 0:
+		host = defaultHost
+	case 1:
+		host = ntpHost[0]
+	default:
+		return nil, fmt.Errorf("too many ntp hosts")
+	}
+
+	resp, err := ntp.Query(host)
+	if err != nil {
+		return nil, err
+	}
+
+	return &networkClock{
+		offset: resp.ClockOffset,
+	}, nil
+}
