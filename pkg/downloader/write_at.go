51d7d13411f9e6de09d428010c9e7916696b7e23
iyear <ljyngup@gmail.com>
2022-10-08 18:39:01 +0800

    fix(downloader): remove duplicate mutex

--- pkg/downloader/write_at.go
+++ pkg/downloader/write_at.go
@@ -3,20 +3,17 @@ package downloader
 import (
 	"github.com/jedib0t/go-pretty/v6/progress"
 	"os"
-	"sync"
 )
 
 // writeAt wrapper for file to use progress bar
+//
+// do not need mutex because gotd has use syncio.WriteAt
 type writeAt struct {
-	mu      sync.Mutex
 	f       *os.File
 	tracker *progress.Tracker
 }
 
 func (w *writeAt) WriteAt(p []byte, off int64) (int, error) {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-
 	at, err := w.f.WriteAt(p, off)
 	if err != nil {
 		w.tracker.MarkAsErrored()
