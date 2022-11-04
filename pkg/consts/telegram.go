502b5d4f928bf6e0e215d68339dcad79cab8fc8a
iyear <ljyngup@gmail.com>
2022-11-04 11:15:50 +0800

    chore(consts): simulate tdesktop application

--- pkg/consts/telegram.go
+++ pkg/consts/telegram.go
@@ -2,7 +2,6 @@ package consts
 
 import (
 	"github.com/gotd/td/telegram"
-	"runtime"
 )
 
 const (
@@ -17,15 +16,18 @@ var Apps = map[string]struct {
 	// application created by iyear
 	AppBuiltin: {AppID: 15055931, AppHash: "021d433426cbb920eeb95164498fe3d3"},
 	// application created by tdesktop.
-	// https://github.com/telegramdesktop/tdesktop/blob/dev/Telegram/SourceFiles/config.h#L94-L95
-	AppDesktop: {AppID: 17349, AppHash: "344583e45741c457fe1862106095a5eb"},
+	// https://opentele.readthedocs.io/en/latest/documentation/authorization/api/#class-telegramdesktop
+	AppDesktop: {AppID: 2040, AppHash: "b18441a1ff607e10a989891a5462e627"},
 }
 
 var (
 	Device = telegram.DeviceConfig{
-		DeviceModel:   "tdl",
-		SystemVersion: runtime.GOOS,
-		AppVersion:    Version,
+		DeviceModel:    "Desktop",
+		SystemVersion:  "Windows 10",
+		AppVersion:     "4.2.4 x64",
+		LangCode:       "en",
+		SystemLangCode: "en-US",
+		LangPack:       "tdesktop",
 	}
 )
 
